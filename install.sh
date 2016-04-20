99ba90228912fb349fe35e31dc8c8cb4d94a2b90
Julio Cesar Martin <juliocesar.martin@bbva.com>
2016-04-20 14:27:13 +0200

    refactor

--- install.sh
+++ install.sh
@@ -1,21 +1,33 @@
+#!/bin/bash
+
 # user
-username=whoami
-atomDir = /home/$username/.atom
+username=$(whoami)
+atomDir=/home/$username/.atom
 # move to my home fodler
 cd /home/$username
 
-echo "Downloading repo... "
+echo "Downloading temp repo... "
 git clone https://github.com/juliomatcom/atom-configuration-pkgs.git
 
-apm install --packages-file atom-configuration-pkgs/packages.txt
+filename=atom-configuration-pkgs/packages.txt
 
-echo "Copy configurations files to ${atomDir}"
+#Install missing packages only
+while read package
+do
+  directory="$atomDir/packages/$package"
+  echo $directory
+  if [ ! -d $directory ]; then
+    echo "apm install $package"
+    apm install $package
+  fi
+done < $filename
+
+echo "Copy configurations files to $atomDir"
 #copy files
-cp atom-configuration-pkgs/init.coffee  $atomDir/init.coffee
-cp atom-configuration-pkgs/keymap.cson $atomDir/keymap.cson
-cp atom-configuration-pkgs/toolbar.cson $atomDir/toolbar.cson
-
+cp atom-configuration-pkgs/confs/. $atomDir/.
 
 # remove repo
-echo "Removing extra files..."
+echo "Removing temp repo..."
 rm -rf atom-configuration-pkgs
+
+echo "done"
