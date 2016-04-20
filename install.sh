ff29386685ac6929f1c132971f94e88ca49ffd9c
Julio Cesar Martin <juliocesar.martin@bbva.com>
2016-04-20 14:34:43 +0200

    syntax

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ atomDir=/home/$username/.atom
 # move to my home fodler
 cd /home/$username
 
-echo "Downloading temp repo... "
+echo "Downloading temp repo.."
 git clone https://github.com/juliomatcom/atom-configuration-pkgs.git
-
+echo "======================================"
 filename=atom-configuration-pkgs/packages.txt
 
-#Install missing packages only
+echo "Install missing packages"
 while read package
 do
   directory="$atomDir/packages/$package"
@@ -21,13 +21,15 @@ do
     apm install $package
   fi
 done < $filename
+echo "======================================"
 
 echo "Copy configurations files to $atomDir"
 #copy files
-cp atom-configuration-pkgs/confs/. $atomDir/.
+cp atom-configuration-pkgs/confs/* $atomDir/.
+echo "======================================"
 
 # remove repo
 echo "Removing temp repo..."
 rm -rf atom-configuration-pkgs
-
+echo "======================================"
 echo "done"
